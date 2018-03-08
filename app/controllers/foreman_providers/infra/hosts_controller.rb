5beb3404bab2e02586b36a4adf32a477d47c6099
Adam Grare <agrare@redhat.com>
2018-03-08 14:28:52 -0500

    Fix namespacing for controllers/helpers/views

---
+++ app/controllers/foreman_providers/infra/hosts_controller.rb
@@ -0,0 +1,13 @@
+module ForemanProviders
+  module Infra
+    # Example: Plugin's HostsController inherits from Foreman's HostsController
+    class HostsController < ::HostsController
+      # change layout if needed
+      # layout 'foreman_providers_infra/layouts/new_layout'
+
+      def new_action
+        # automatically renders view/foreman_providers_infra/hosts/new_action
+      end
+    end
+  end
+end
